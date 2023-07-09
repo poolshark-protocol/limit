d8a6f7f5e3bab5f66d839b3b738d14e2c7f23ab8
alphak3y <alphak3y@protonmail.com>
2023-07-09 09:45:31 -0600

    add ts files

---
+++ taskHandler.ts
@@ -0,0 +1,45 @@
+import { SUPPORTED_NETWORKS } from './scripts/constants/supportedNetworks'
+import {
+    DEPLOY_COVERPOOL,
+    DEPLOY_COVERPOOLS,
+    INCREASE_SAMPLES,
+    MINT_POSITION,
+    MINT_TOKENS,
+    VERIFY_CONTRACTS,
+} from './tasks/constants/taskNames'
+import { purpleLog } from './test/utils/colors'
+
+export function handleHardhatTasks() {
+    handleLimitPoolTasks()
+}
+
+function handleLimitPoolTasks() {
+    // for (const network in SUPPORTED_NETWORKS) {
+    //     if (Object.keys(LOCAL_NETWORKS).includes(network)) continue;
+    //     hre.masterNetwork = MASTER_NETWORKS[network];
+    //     break;
+    // }
+    if (process.argv.includes(DEPLOY_COVERPOOLS)) {
+        import('./tasks/deploy/deploy-coverpools')
+        logTask(DEPLOY_COVERPOOLS)
+    } else if (process.argv.includes(DEPLOY_COVERPOOL)) {
+        import('./tasks/deploy/deploy-coverpool')
+        logTask(DEPLOY_COVERPOOL)
+    } else if (process.argv.includes(INCREASE_SAMPLES)) {
+        import('./tasks/deploy/increase-samples')
+        logTask(INCREASE_SAMPLES)
+    } else if (process.argv.includes(MINT_TOKENS)) {
+        import('./tasks/deploy/mint-tokens')
+        logTask(MINT_TOKENS)
+    } else if (process.argv.includes(MINT_POSITION)) {
+        import('./tasks/deploy/mint-position')
+        logTask(MINT_POSITION)
+    } else if (process.argv.includes(VERIFY_CONTRACTS)) {
+        import('./tasks/deploy/verify-contracts')
+        logTask(VERIFY_CONTRACTS)
+    }
+}
+
+function logTask(taskName: string) {
+    purpleLog(`\n🎛  Running ${taskName} task...\n`)
+}

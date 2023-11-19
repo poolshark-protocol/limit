6b68bacdfffe5542f1a0a527131f26dbc195b9ef
alphak3y <alphak3y@protonmail.com>
2023-11-19 03:33:55 -0700

    router deployment

--- scripts/autogen/contract-deployments-keys.ts
+++ scripts/autogen/contract-deployments-keys.ts
@@ -45,4 +45,8 @@ export const CONTRACT_DEPLOYMENT_KEYS: ContractDeploymentsKey[] = [
         networkName: 'arb_goerli',
         objectName: 'poolRouter'
     },
+    {
+        networkName: 'arb_goerli',
+        objectName: 'poolRouter'
+    },
 ];

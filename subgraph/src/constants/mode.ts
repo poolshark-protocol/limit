864b315e372ff486a9b0ba479ff4d05fc79af589
alphak3y <alphak3y@protonmail.com>
2024-02-07 14:30:25 -0700

    add stable pool; start times

--- subgraph/src/constants/mode.ts
+++ subgraph/src/constants/mode.ts
@@ -24,15 +24,14 @@ export let WHITELISTED_TOKENS: string[] = [
   '0xe7798f023fc62146e8aa1b36da45fb70855a77ea', // DAI
 ]
 
-//TODO: incentivizing only FIN - WETH 0.3% for now
-
 export let WHITELISTED_PAIRS: string[] = [
   '0xb5fd40e12a35c6afe8dcc48544082ef2cc371aa5', // WETH-USDT 0.1%
   '0xe0691e6803d4fa0d8fa8ee8da7667eb4a6b99415', // WETH-USDC 0.1%
+  '0x1a4cadc783f06829df1cff5db0df7288d716c5a1' // USDC-USDT 0.1%
 ]
 
-export let SEASON_1_START_TIME = BigInt.fromString('0') // 2-09-2024 15:00 GMT - 1707516000
-export let SEASON_1_END_TIME = BigInt.fromString('1710021600') // 3-09-2024 15:00 GMT - 1710021600
+export let SEASON_1_START_TIME = BigInt.fromString('1707490800') // 2-09-2024 15:00 GMT - 1707490800
+export let SEASON_1_END_TIME = BigInt.fromString('1709996400') // 3-09-2024 15:00 GMT - 1709996400
 
 // used for safe eth pricing 
 export let STABLE_COINS: string[] = [
